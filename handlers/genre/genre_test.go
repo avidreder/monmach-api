66a662e4778597ef6ddc58425da2a716261f268e
Andrew Reder <avidreder@gmail.com>
2016-12-01 20:32:07 -0800

    Got test for store create working

--- handlers/genre/genre_test.go
+++ handlers/genre/genre_test.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+  "net/url"
+  "strings"
 	"testing"
+  "log"
 
 	"github.com/avidreder/monmach-api/handlers/genre"
   genreR "github.com/avidreder/monmach-api/resources/genre"
@@ -34,11 +37,30 @@ func (s *GenreTestSuite) SetupTest() {
 }
 
 func (s *GenreTestSuite) TestCreateOK() {
+  f := make(url.Values)
+  f.Set("UserID", "1")
+  f.Set("QueueID", "1")
+  f.Set("Name", "test")
+  f.Set("Description", "test")
+  f.Set("SeedArtists", "[]")
+  f.Set("SeedTracks", "[]")
+  f.Set("SeedPlaylists", "[]")
+  f.Set("AvatarURL", "url")
+  f.Set("TrackBlacklist", "[]")
+  f.Set("TrackWhitelist", "[]")
+  req, err := http.NewRequest(echo.POST, "/", strings.NewReader(f.Encode()))
+  req.Form = f
+  req.PostForm = f
+  log.Printf("%+v",req)
+  s.NoError(err)
+  e := echo.New()
+  rec := httptest.NewRecorder()
+  c := e.NewContext(req, rec)
   mockStore := mocks.Store{}
   mockStore.On("Create",mock.Anything).Return(nil)
-  s.c.Set("store", mockStore)
+  c.Set("store", mockStore)
   testGenreJSON, _ := json.Marshal(genreR.Genre{
-    ID: 1,
+    ID: 0,
   	UserID: 1,
   	QueueID:1,
   	Name: "test",
@@ -50,10 +72,10 @@ func (s *GenreTestSuite) TestCreateOK() {
   	TrackBlacklist: []int64{},
   	TrackWhitelist: []int64{},
   })
-  err := genre.Create(s.c)
+  err = genre.Create(c)
   s.NoError(err)
-  s.Equal(http.StatusOK, s.rec.Code)
-  s.Equal(testGenreJSON, s.rec.Body.String())
+  s.Equal(http.StatusOK, rec.Code)
+  s.Equal(string(testGenreJSON), rec.Body.String())
 }
 
 func TestGenreTestSuite(t *testing.T) {
--- test/mocks/store.go
+++ test/mocks/store.go
@@ -13,35 +13,35 @@ type Store struct {
 // QueryBIT mocks the associated store function
 func (m Store) Connect() error {
   args := m.Called()
-	return args.Error(1)
+	return args.Error(0)
 }
 
 // Get mocks the associated store function
 func (m Store) Get(interface{}) error {
 	args := m.Called()
-	return args.Error(1)
+	return args.Error(0)
 }
 
 // Update mocks the associated store function
 func (m Store) Update(string, int64, map[string]interface{}) error {
 	args := m.Called()
-	return args.Error(1)
+	return args.Error(0)
 }
 
 // Delete mocks the associated store function
 func (m Store) Delete(interface{}) error {
 	args := m.Called()
-	return args.Error(1)
+	return args.Error(0)
 }
 
 // Create mocks the associated store function
 func (m Store) Create(interface{}) error {
 	args := m.Called()
-	return args.Error(1)
+	return args.Error(0)
 }
 
 // GetAll mocks the associated store function
 func (m Store) GetAll(interface{}, string) error {
 	args := m.Called()
-	return args.Error(1)
+	return args.Error(0)
 }
