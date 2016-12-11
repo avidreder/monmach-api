29bb3103707792dd94b0831abede70ff8153e130
Andrew Reder <avidreder@gmail.com>
2016-12-11 10:25:51 -0800

    Added mongo functions

--- resources/store/mongo/mongo.go
+++ resources/store/mongo/mongo.go
@@ -1,18 +1,67 @@
 package mongo
 
 import (
+	"log"
+	"encoding/json"
+
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+var dataStore = Store{}
+
+// Store implements store interface
+type Store struct {
+	mongoSession *mgo.Session
+}
 
 const dbURL = "mongodb://localhost:27017"
-const db = "showhawk"
-const collection = "shows"
+const db = "monmach"
 
-func Connect() (*mgo.Collection, error) {
+func (s Store) Connect() error {
 	session, err := mgo.Dial(dbURL)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	col := session.DB(db).C(collection)
-	return col, nil
+	s.mongoSession = session
+	return nil
+}
+
+func (s Store) GetAll(model interface{}, collection string) error {
+	data, err := s.mongoSession.DB(db).Collection(collection).Find().All(model)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Store) GetByKey(model interface{}, key string, value interface{}) error {
+	err := s.mongoSession.DB(db).Collection(collection).Find(bson.M{key: value}).One(model)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Store) UpdateByKey(model interface{}, key string, value interface{}) error {
+	err := s.mongoSession.DB(db).Collection(collection).Update(bson.M{key: value}, bson.M{"$set":bson.M{model}})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Store) DeleteByKey(model interface{}, key string, value interface{}) error {
+	err := s.mongoSession.DB(db).Collection(collection).Remove{bson.M{key: value})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Store) Create(model interface{}) error {
+	err := s.mongoSession.DB(db).Collection(collection).Insert{model})
+	if err != nil {
+		return err
+	}
+	return nil
 }
